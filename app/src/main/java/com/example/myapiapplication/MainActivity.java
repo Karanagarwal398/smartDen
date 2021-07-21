215bf63b7bd6636f3165e2d2fb9f930178eaaea8
karan Agarwal <karanagarwal398@gmail.com>
2021-07-21 13:09:13 +0530

    first
    First Application

---
+++ app/src/main/java/com/example/myapiapplication/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.myapiapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openResult();
+            }
+        });
+
+    }
+
+    public void openResult() {
+        Intent intent = new Intent(this, Result.class);
+        startActivity(intent);
+    }
+}
